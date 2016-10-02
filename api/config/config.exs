737b6326edd5cfdff001e62452705b4ea38bb27c
Scott Douglass <scott@scottyd.net>
2016-10-01 21:42:12 -0400

    user model

--- api/config/config.exs
+++ api/config/config.exs
@@ -30,6 +30,15 @@ config :mime, :types, %{
   "application/vnd.api+json" => ["json-api"]
 }
 
+config :guardian, Guardian,
+  allowed_algos: ["HS512"], # optional
+  verify_module: Guardian.JWT,  # optional
+  issuer: "Mirror",
+  ttl: { 30, :days },
+  verify_issuer: true, # optional
+  secret_key: System.get_env("GUARDIAN_SECRET") || "nGeq4o2xpePnwn46bhB4ItyiuiwM3doUuWi5T7yprcbWtF0xdpB5DqjkAEa6/2a/",
+  serializer: Mirror.GuardianSerializer
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
---
+++ api/web/models/user.ex
@@ -0,0 +1,36 @@
+defmodule Mirror.User do
+  use Mirror.Web, :model
+
+  schema "users" do
+    field :username, :string
+    field :email, :string
+    field :password_hash, :string
+
+    # Two virtual fields for password confirmation
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:username, :email, :password, :password_confirmation])
+    |> validate_required([:username, :email, :password, :password_confirmation])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 6)
+    |> validate_confirmation(:password)
+    |> hash_password()
+    |> unique_constraint(:email)
+    |> unique_constraint(:username)
+  end
+
+  defp hash_password(%{valid?: false} = changeset), do: changeset
+  defp hash_password(%{valid?: true} = changeset) do
+      hashedpw = Comeonin.Bcrypt.hashpwsalt(Ecto.Changeset.get_field(changeset, :password))
+      Ecto.Changeset.put_change(changeset, :password_hash, hashedpw)
+  end
+end
