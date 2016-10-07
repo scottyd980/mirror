c8d2dfb288a678b86507d3b8e0afd7688e8696f2
Scott Douglass <scott@scottyd.net>
2016-10-06 21:38:49 -0400

    fixing navbar / making it close on item selection

--- ui/app/pods/application/controller.js
+++ ui/app/pods/application/controller.js
@@ -2,4 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
+  actions: {
+    navbarCollapse(target) {
+
+      console.log('test');
+      // Do not collapse on desktop (i.e. if the button navbar toggle is hidden)
+      if (Ember.$('button.navbar-toggle').is(':hidden')) {
+        return;
+      }
+      // Do not collapse if target is a sub menu
+      if (Ember.$(target).hasClass('dropdown-toggle')) {
+        return;
+      }
+
+      Ember.$('.navbar-collapse').collapse('hide');
+    }
+  }
 });
