4f79680312dadce48207f5e74048d9201df74ba8
Scott Douglass <scott@scottyd.net>
2016-10-09 22:01:22 -0400

    updating models to add new relationships for client

---
+++ ui/app/pods/team/model.js
@@ -0,0 +1,11 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
+import { belongsTo } from 'ember-data/relationships';
+
+export default Model.extend({
+  name: attr('string'),
+  avatar: attr('string'),
+  admin: belongsTo('user'),
+  members: hasMany('user'),
+});
