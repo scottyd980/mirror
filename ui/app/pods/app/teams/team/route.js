828e20975bd8ceac02400e83da03717f93c92965
Scott Douglass <scott@scottyd.net>
2016-10-17 21:40:00 -0400

    protecting team route and handling error on ui

--- ui/app/pods/app/teams/team/route.js
+++ ui/app/pods/app/teams/team/route.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
+  model(params) {
+    var _this = this;
+    return RSVP.hash({
+      team: _this.get('store').findRecord('team', params.id)
+    });
+  },
+  setupController(controller, model) {
+    console.log(model);
+  },
+  actions: {
+    error(error, transition) {
+      if(error) {
+        this.transitionTo('error');
+      }
+    }
+  }
 });
