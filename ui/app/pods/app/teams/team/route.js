6569c2dad98beddf6079a6de24000e829823d481
Scott Douglass <scott@scottyd.net>
2016-10-19 20:14:59 -0400

    fixed error handling more

--- ui/app/pods/app/teams/team/route.js
+++ ui/app/pods/app/teams/team/route.js
@@ -4,11 +4,8 @@ import RSVP from 'rsvp';
 export default Ember.Route.extend({
   model(params) {
     var _this = this;
-    return RSVP.hash({
-      team: _this.get('store').findRecord('team', params.id).catch(() => { throw new Error('The team you were looking for was not found.') })
+    return _this.get('store').findRecord('team', params.id).catch(() => {
+      throw {type: "404", description: "Page Not Found", message: "We're sorry... we searched all over but couldn't find the page you were looking for!"};
     });
-  },
-  setupController(controller, model) {
-
   }
 });
