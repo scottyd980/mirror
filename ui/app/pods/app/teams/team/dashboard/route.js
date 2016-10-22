cc35d3e82d514f3d8ae29276a1912eb93ada6ecb
Scott Douglass <scott@scottyd.net>
2016-10-22 19:28:11 -0400

    added preferences/admin pane

--- ui/app/pods/app/teams/team/dashboard/route.js
+++ ui/app/pods/app/teams/team/dashboard/route.js
@@ -2,9 +2,18 @@ import Ember from 'ember';
 import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
+  session: Ember.inject.service('session'),
   model() {
+    var _this = this;
     return RSVP.hash({
-      team: this.modelFor('app.teams.team')
+      team: this.modelFor('app.teams.team'),
+      currentUser: _this.get('session').get('currentUser')
     });
+  },
+  setupController(controller, models) {
+    var _this = this;
+    _this._super(controller, models);
+
+    controller.set('isAdmin', models.currentUser.get('id') === models.team.get('admin.id'));
   }
 });
