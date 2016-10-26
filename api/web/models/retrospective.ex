ac16aa85f78f3453665b5831ca5fdc2f917dc013
Scott Douglass <scott@scottyd.net>
2016-10-25 20:52:50 -0400

    added retrospectives model and migration

---
+++ api/web/models/retrospective.ex
@@ -0,0 +1,24 @@
+defmodule Mirror.Retrospective do
+  use Mirror.Web, :model
+
+  schema "retrospectives" do
+    field :name, :string
+    field :state, :integer, default: 0
+    field :isAnonymous, :boolean, default: true
+    belongs_to :team, Mirror.Team
+    belongs_to :moderator, Mirror.User
+    # type
+    # many_to_many :participants, Mirror.User, join_through: Mirror.UserTeam
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name, :state, :isAnonymous])
+    |> validate_required([:name, :state, :isAnonymous])
+  end
+end
