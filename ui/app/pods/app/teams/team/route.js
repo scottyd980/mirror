19cd531ab7b160bd78a1d1b0a8413561074a5b24
Scott Douglass <scott@scottyd.net>
2016-11-04 19:24:54 -0400

    marking member_delegates as accessed

--- ui/app/pods/app/teams/team/route.js
+++ ui/app/pods/app/teams/team/route.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 import RSVP from 'rsvp';
+import config from '../../../../config/environment';
 
 export default Ember.Route.extend({
   model(params) {
     var _this = this;
     return _this.get('store').findRecord('team', params.id).catch(() => {
-      throw {type: "404", description: "Page Not Found", message: "We're sorry... we searched all over but couldn't find the page you were looking for!"};
+      throw config.ERROR_CODES.not_found;
     });
   }
 });
