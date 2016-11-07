2aedb3f9507a98b4da990ee3c821d71612ad44e6
Scott Douglass <scott@scottyd.net>
2016-11-07 11:44:37 -0500

    checking around removing admins from the team and admins leaving the team

--- api/web/controllers/user_team_controller.ex
+++ api/web/controllers/user_team_controller.ex
@@ -5,6 +5,7 @@ defmodule Mirror.UserTeamController do
   alias Mirror.Team
   alias Mirror.UserTeam
   alias Mirror.MemberDelegate
+  alias Mirror.TeamAdmin
 
   import Logger
 
@@ -33,12 +34,30 @@ defmodule Mirror.UserTeamController do
     user = Repo.get!(User, user_id)
 
     cond do
-      user_is_member?(user, team) ->
-        handle_remove_member(conn, %{user_id: user_id, team_id: team_id})
       user_is_admin?(user, team) ->
-        handle_remove_member(conn, %{user_id: user_id, team_id: team_id})
+        case handle_remove_admin_member(%{user: user, team: team}) do
+          {:ok, {1, 1}} ->
+            conn
+            |> put_status(:ok)
+            |> render(Mirror.UserTeamView, "delete.json", user_team: %{user_id: user.id, team_id: team.id})
+          {:error, "No other admins"} ->
+            use_error_view(conn, :forbidden, %{})
+          {:error, "Unexpected error"} ->
+            use_error_view(conn, 500, %{})
+          {:error, changeset} ->
+            use_error_view(conn, :unprocessable_entity, changeset)
+        end
+      user_is_member?(user, team) ->
+        case handle_remove_member(%{user: user, team: team}) do
+          {:ok, affected_rows} ->
+            conn
+            |> put_status(:ok)
+            |> render(Mirror.UserTeamView, "delete.json", user_team: %{user_id: user.id, team_id: team.id})
+          {:error, _} ->
+            use_error_view(conn, 422, %{})
+        end
       true ->
-        unprocessable_response(conn, %{})
+        use_error_view(conn, 422, %{})
     end
   end
 
@@ -83,25 +102,54 @@ defmodule Mirror.UserTeamController do
         |> put_status(:created)
         |> render(Mirror.UserTeamView, "show.json", user_team: user_team)
       {:error, changeset} ->
-        unprocessable_response(conn, changeset)
+        use_error_view(conn, 422, changeset)
     end
 
   end
 
-  defp handle_remove_member(conn, user_team) do
-    case Repo.delete_all(from u in UserTeam, where: u.user_id == ^user_team.user_id and u.team_id == ^user_team.team_id) do
+  defp handle_remove_admin_member(user_team) do
+    team = user_team.team
+    |> Repo.preload([:admins])
+
+    cond do
+      (length(team.admins) > 1) ->
+        Repo.transaction fn ->
+          with {:ok, member_affected_rows} <- handle_remove_member(user_team),
+               {:ok, admin_affected_rows} <- handle_remove_admin(user_team) do
+                 {member_affected_rows, admin_affected_rows}
+          else
+            {:error, changeset} ->
+              Repo.rollback changeset
+          end
+        end
+      (length(team.admins) > 0) ->
+        {:error, "No other admins"}
+      true ->
+        {:error, "Unexpected error"}
+    end
+  end
+
+  defp handle_remove_admin(user_team) do
+    case Repo.delete_all(from u in TeamAdmin, where: u.user_id == ^user_team.user.id and u.team_id == ^user_team.team.id) do
       {:error, _} ->
-        unprocessable_response(conn, %{})
-      {_, _} ->
-        conn
-        |> put_status(:ok)
-        |> render(Mirror.UserTeamView, "delete.json", user_team: user_team)
+        {:error, "Problem deleting rows"}
+      {affected_rows, _} ->
+        {:ok, affected_rows}
     end
   end
 
-  defp unprocessable_response(conn, changeset) do
+  defp handle_remove_member(user_team) do
+    case Repo.delete_all(from u in UserTeam, where: u.user_id == ^user_team.user.id and u.team_id == ^user_team.team.id) do
+      {:error, _} ->
+        {:error, "Problem deleting rows"}
+      {affected_rows, _} ->
+        {:ok, affected_rows}
+    end
+  end
+
+  defp use_error_view(conn, status, changeset) do
     conn
-    |> put_status(:unprocessable_entity)
+    |> put_status(status)
     |> render(Mirror.ChangesetView, "error.json", changeset: changeset)
   end
 end
