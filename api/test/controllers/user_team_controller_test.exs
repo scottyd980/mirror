7b03167da7b6bf80ee10733512ff5d2634d5f846
Scott Douglass <scott@scottyd.net>
2016-11-13 19:59:38 -0500

    improving unit tests

--- api/test/controllers/user_team_controller_test.exs
+++ api/test/controllers/user_team_controller_test.exs
@@ -4,6 +4,7 @@ defmodule Mirror.UserTeamControllerTest do
   alias Mirror.Team
   alias Mirror.User
   alias Mirror.UserTeam
+  alias Mirror.TeamAdmin
   alias Mirror.MemberDelegate
 
   import Logger
@@ -14,6 +15,10 @@ defmodule Mirror.UserTeamControllerTest do
 
   defp add_as_team_member(user, team) do
     Repo.insert! %UserTeam{team_id: team.id, user_id: user.id}
+  end
+
+  defp add_as_team_admin(user, team) do
+    Repo.insert! %TeamAdmin{team_id: team.id, user_id: user.id}
   end
 
   setup %{conn: conn} do
@@ -53,6 +58,51 @@ defmodule Mirror.UserTeamControllerTest do
         "user_id" => user.id
     	}
     }
+  end
+
+  test "deletes a user and returns a 200 when data is a valid team member", %{conn: conn, user: user} do
+    team = Repo.insert! %Team{}
+    add_as_team_member(user, team)
+
+    conn = delete conn, user_team_path(conn, :delete), %{"user_id": user.id, "team_id": team.id}
+    assert json_response(conn, 200) == %{"meta" => %{}}
+  end
+
+  test "deletes a user who is an admin and returns a 200 when user is a valid team member and is not the only admin", %{conn: conn, user: user} do
+    # Create a team and add the current user as a team member and team admin
+    team = Repo.insert! %Team{}
+    add_as_team_member(user, team)
+    add_as_team_admin(user, team)
+
+    # Add an additional admin to the team
+    new_user = Repo.insert! %User{}
+    add_as_team_member(new_user, team)
+    add_as_team_admin(new_user, team)
+
+    conn = delete conn, user_team_path(conn, :delete), %{"user_id": user.id, "team_id": team.id}
+    assert json_response(conn, 200) == %{"meta" => %{}}
+  end
+
+  test "does not delete a user who is the only remaining admin and returns a 403", %{conn: conn, user: user} do
+    # Create a team and add the current user as a team member and team admin
+    team = Repo.insert! %Team{}
+    add_as_team_member(user, team)
+    add_as_team_admin(user, team)
+
+    conn = delete conn, user_team_path(conn, :delete), %{"user_id": user.id, "team_id": team.id}
+    assert json_response(conn, 403) == %{"errors" => []}
+  end
+
+  test "user, who is not a member of the team, tries to remove a member of the team and returns a 401", %{conn: conn, user: user} do
+    # Create a team but don't add the current logged in user to it
+    team = Repo.insert! %Team{}
+
+    # Add an additional user to be added the team
+    new_user = Repo.insert! %User{}
+    add_as_team_member(new_user, team)
+
+    conn = delete conn, user_team_path(conn, :delete), %{"user_id": new_user.id, "team_id": team.id}
+    assert json_response(conn, 401) == %{"errors" => []}
   end
 
 end
