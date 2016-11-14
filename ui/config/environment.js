8e2f018df257f129123a6de738214971f32b4a4c
Scott Douglass <scott@scottyd.net>
2016-11-13 20:59:17 -0500

    added more specific messages for confirmations

--- ui/config/environment.js
+++ ui/config/environment.js
@@ -64,7 +64,7 @@ module.exports = function(environment) {
       generic: "Success!"
     },
     CONFIRM_MESSAGES: {
-      generic: "Are You Sure?"
+      generic: "Please Confirm This Action"
     }
   };
 
