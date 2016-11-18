824cf4895d8ceabc5175df384088d9e9642e82b0
Scott Douglass <scott@scottyd.net>
2016-11-17 19:47:49 -0500

    added user helper along with validation on the team channel to make sure the user is a team member

---
+++ api/lib/user_helper.ex
@@ -0,0 +1,18 @@
+defmodule Mirror.UserHelper do
+  alias Mirror.Repo
+  alias Mirror.Team
+
+  def user_is_member?(user, team) do
+    team = Repo.get!(Team, team.id)
+    |> Repo.preload([:admins, :members])
+
+    Enum.member?(team.members, user)
+  end
+
+  def user_is_admin?(user, team) do
+    team = Repo.get!(Team, team.id)
+    |> Repo.preload([:admins, :members])
+
+    Enum.member?(team.admins, user)
+  end
+end
