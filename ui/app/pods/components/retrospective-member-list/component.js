75fb095ac6d73529824d46c55625a3f5dfaa5df6
Scott Douglass <scott@scottyd.net>
2016-12-12 15:55:27 -0500

    Added start page to retrospective. Includes a list of members who have joined an who we're waiting on.

---
+++ ui/app/pods/components/retrospective-member-list/component.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: "ul",
+  classNames: ["action-list"],
+  sortedMembers: Ember.computed.sort('members', 'memberSortDefinition'),
+  memberSortDefinition: ['id:asc']
+});
