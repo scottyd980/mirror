c003ff43a7c61bff8c01950e6215e8d7640ee144
Scott Douglass <scott@scottyd.net>
2016-12-12 21:47:50 -0500

    Handling updating in retrospective.
    Still running into some issues with ember updating relationships though.

--- api/web/controllers/retrospective_user_controller.ex
+++ api/web/controllers/retrospective_user_controller.ex
@@ -11,18 +11,18 @@ defmodule Mirror.RetrospectiveUserController do
 
   plug Guardian.Plug.EnsureAuthenticated, handler: Mirror.AuthErrorHandler
 
-  def create(conn, %{"user_id" => user_id, "retrospective_id" => retrospective_id}) do
+  def create(conn, %{"retrospective_id" => retrospective_id}) do
     current_user = Guardian.Plug.current_resource(conn)
 
     retrospective = Repo.get!(Retrospective, retrospective_id)
-    |> Repo.preload([:team])
+    |> Repo.preload([:team, :moderator, :type, :participants])
 
     team = retrospective.team
     |> Repo.preload([:members])
 
     cond do
       UserHelper.user_is_team_member?(current_user, team) ->
-        handle_add_retrospective_participant(conn, user, retrospective)
+        handle_add_retrospective_participant(conn, current_user, retrospective)
       true ->
         use_error_view(conn, 401, %{})
     end
@@ -32,10 +32,12 @@ defmodule Mirror.RetrospectiveUserController do
   defp handle_add_retrospective_participant(conn, user, retrospective) do
 
     changeset = %RetrospectiveUser{}
-    |> RetrospectiveUser.changeset(%{user_id: user.id, retrospective_id: retrospective.id)
+    |> RetrospectiveUser.changeset(%{user_id: user.id, retrospective_id: retrospective.id})
     
     case Repo.insert changeset do
-        {:ok, team_admin} ->
+        {:ok, retrospective_user} ->
+            Mirror.Endpoint.broadcast("retrospective:#{retrospective.id}", "joined_retrospective", Mirror.RetrospectiveView.render("show.json", retrospective: retrospective))
+
             conn
             |> put_status(:created)
             |> render(Mirror.RetrospectiveUserView, "show.json", retrospective_user: retrospective_user)
--- ui/app/pods/retrospective-service/service.js
+++ ui/app/pods/retrospective-service/service.js
@@ -3,11 +3,15 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   socket: Ember.inject.service('socket-service'),
   routing: Ember.inject.service("-routing"),
-  channel: null,
+  store: Ember.inject.service('store'),
+
+  team_channel: null,
+  retrospective_channel: null,
   pre_retrospective: {
     in_progress: false,
     retrospective_id: null
   },
+
   init() {
     this._super(...arguments);
     this.set('channel', null);
@@ -16,17 +20,17 @@ export default Ember.Service.extend({
     return retrospective.save();
   },
   lookup_in_progress(team_id) {
-    this.get('channel').push('check_retrospective_in_progress', {});
+    this.get('team_channel').push('check_retrospective_in_progress', {});
   },
   reset_pre_retrospective() {
     this.set('pre_retrospective', {
       in_progress: false,
       retrospective_id: null
     });
-    this.set('channel', null);
+    this.set('team_channel', null);
   },
-  sendMessage(topic, data) {
-    this.get('channel').push(topic, data);
+  sendMessage(channel_type, topic, data) {
+    this.get(`${channel_type}_channel`).push(topic, data);
   },
 
   // Team
@@ -36,7 +40,7 @@ export default Ember.Service.extend({
     var team_retrospective_channel = this.get('socket').joinChannel(`team:${team_id}`);
     team_retrospective_channel.then((chan) => {
       this.listen_for_team_events(chan);
-      this.set('channel', chan);
+      this.set('team_channel', chan);
       this.lookup_in_progress(team_id);
     });
 
@@ -59,8 +63,7 @@ export default Ember.Service.extend({
     var retrospective_channel = this.get('socket').joinChannel(`retrospective:${retrospective_id}`);
     retrospective_channel.then((chan) => {
       this.listen_for_retrospective_events(chan);
-      this.set('channel', chan);
-      this.lookup_in_progress(retrospective_id);
+      this.set('retrospective_channel', chan);
     });
   },
 
@@ -70,7 +73,10 @@ export default Ember.Service.extend({
 
   _listen_for_joined_retrospective(channel) {
     channel.on('joined_retrospective', (resp) => {
-      this.get('routing').transitionTo('app.retrospectives.retrospective.start', [resp.retrospective_id]);
+      this.get('store').push(resp);
+      console.log(resp);
+      // console.log('someone joined');
+      // console.log(resp);
     });
   }
 });
