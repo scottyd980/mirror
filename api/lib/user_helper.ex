7fbb17670d25720e207b6f908348b7586fe49bad
Scott Douglass <scott@scottyd.net>
2016-12-14 20:19:57 -0500

    No longer add existing participants over and over.

--- api/lib/user_helper.ex
+++ api/lib/user_helper.ex
@@ -1,6 +1,7 @@
 defmodule Mirror.UserHelper do
   alias Mirror.Repo
   alias Mirror.Team
+  alias Mirror.Retrospective
 
   def user_is_team_member?(user, team) do
     team = Repo.get!(Team, team.id)
@@ -14,5 +15,12 @@ defmodule Mirror.UserHelper do
     |> Repo.preload([:admins, :members])
 
     Enum.member?(team.admins, user)
+  end
+
+  def user_is_participant?(user, retrospective) do
+    retro = Repo.get!(Retrospective, retrospective.id)
+    |> Repo.preload([:participants])
+
+    Enum.member?(retro.participants, user)
   end
 end
