7cd60a07153f630a2ad7043eccf5f720ac744f47
Scott Douglass <scott@scottyd.net>
2017-01-08 16:31:26 -0500

    Added sprint score controller and ui model

---
+++ api/web/controllers/sprint_score_controller.ex
@@ -0,0 +1,59 @@
+defmodule Mirror.SprintScoreController do
+  use Mirror.Web, :controller
+
+  alias Mirror.User
+  alias Mirror.Retrospective
+  alias Mirror.UserHelper
+  alias Mirror.SprintScore
+
+  plug Guardian.Plug.EnsureAuthenticated, handler: Mirror.AuthErrorHandler
+
+  def create(conn, %{"data" => %{"attributes" => attributes, "relationships" => relationships, "type" => "scores"}}) do
+
+    current_user = Guardian.Plug.current_resource(conn)
+
+    # TODO: need to handle making sure user is retro/team member
+
+    changeset = SprintScore.changeset %SprintScore{}, %{
+        score: attributes.score,
+        user_id: relationships.data.user.id,
+        retrospective_id: relationships.retrospective.data.id
+    }
+
+    case Repo.insert changeset do
+        {:ok, score} ->
+            conn
+            |> put_status(:created)
+            |> render(Mirror.SprintScoreView, "show.json", score: score)
+        {:error, changeset} ->
+            use_error_view(conn, 422, changeset)
+    end
+
+  end
+
+  def show(conn, %{"id" => id}) do
+    current_user = Guardian.Plug.current_resource(conn)
+
+    score = Repo.get!(SprintScore, id)
+    |> Repo.preload([:user, :retrospective])
+
+    retrospective = score.retrospective
+    |> Repo.preload([:team])
+
+    team = retrospective.team
+    |> Repo.preload([:members])
+
+    case UserHelper.user_is_team_member?(current_user, team) do
+      true ->
+        render(conn, "show.json", score: score)
+      _ ->
+        use_error_view(conn, 401, %{})
+    end
+  end
+
+  defp use_error_view(conn, status, changeset) do
+    conn
+    |> put_status(status)
+    |> render(Mirror.ChangesetView, "error.json", changeset: changeset)
+  end
+end
