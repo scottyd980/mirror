a9dee066f1f5bf4cc4ba2ceb4effa078a77ff60e
Scott Douglass <scott@scottyd.net>
2017-01-15 20:30:08 -0500

    Updating continue to share actions

--- ui/app/pods/app/retrospectives/retrospective/start/route.js
+++ ui/app/pods/app/retrospectives/retrospective/start/route.js
@@ -3,12 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
         return this.modelFor('app.retrospectives.retrospective')
-    },
-    actions: {
-        startRetrospective() {
-            var retrospective = this.get('currentModel').retrospective;
-            retrospective.set('state', 1);
-            retrospective.save();
-        }
     }
 });
