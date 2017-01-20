ef6b71ec3d020aa9a73867425fe6789ed2b52421
Scott Douglass <scott@scottyd.net>
2017-01-20 12:45:30 -0500

    Began working on the aggregate page

--- ui/app/pods/app/retrospectives/retrospective/aggregate/route.js
+++ ui/app/pods/app/retrospectives/retrospective/aggregate/route.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
+import config from '../../../../../config/environment';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
+    model() {
+        return RSVP.hash({
+            parent: this.modelFor('app.retrospectives.retrospective'),
+            scores: this.modelFor('app.retrospectives.retrospective').retrospective.get('scores')
+        });
+    }
 });
