da57f0e167d39fb82ae0d8e1b46e977db8ab9420
Scott Douglass <scott@scottyd.net>
2017-01-20 12:18:07 -0500

    Fixing retrospective -> retrospective_id

--- api/web/controllers/retrospective_controller.ex
+++ api/web/controllers/retrospective_controller.ex
@@ -47,7 +47,7 @@ defmodule Mirror.RetrospectiveController do
       UserHelper.user_is_team_member?(current_user, team) ->
         case create_retrospective(params) do
           {:ok, retrospective} ->
-            Mirror.Endpoint.broadcast("team:#{retrospective.team.id}", "retrospective_in_progress", %{retrospective_in_progress: true, retrospective: retrospective.id})
+            Mirror.Endpoint.broadcast("team:#{retrospective.team.id}", "retrospective_in_progress", %{retrospective_in_progress: true, retrospective_id: retrospective.id})
 
             conn
             |> put_status(:created)
