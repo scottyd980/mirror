3bff7d0d502e496bdac5763e4043be55aa8eb6ba
Scott Douglass <scott@scottyd.net>
2017-01-20 20:49:16 -0500

    Added low, average, and high scores

--- ui/app/pods/app/retrospectives/retrospective/aggregate/route.js
+++ ui/app/pods/app/retrospectives/retrospective/aggregate/route.js
@@ -8,5 +8,18 @@ export default Ember.Route.extend({
             parent: this.modelFor('app.retrospectives.retrospective'),
             scores: this.modelFor('app.retrospectives.retrospective').retrospective.get('scores')
         });
+    },
+    setupController(controller, model) {
+        this._super(...arguments);
+
+        let scores = model.scores.sortBy('score');
+
+        let average = scores.reduce((total, score) => {
+            return total + score.get('score');
+        }, 0) / scores.length;
+
+        controller.set('low_score', scores[0]);
+        controller.set('high_score', scores[scores.length - 1]);
+        controller.set('average_score', average);
     }
 });
