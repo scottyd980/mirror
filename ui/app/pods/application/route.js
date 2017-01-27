0f5ded3ae3adc0becd42f0d976aaba5727852f54
Scott Douglass <scott@scottyd.net>
2017-01-26 21:18:06 -0500

    Added team sort so it is consistent when loaded

--- ui/app/pods/application/route.js
+++ ui/app/pods/application/route.js
@@ -14,7 +14,7 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
     _this._super(controller, models);
 
     if(_this.get('session.isAuthenticated')) {
-      models.filteredTeams = _this.get('session.currentUser').get('teams');
+      models.filteredTeams = _this.get('session.currentUser').get('teams').sortBy('id');
     }
 
     controller.setProperties(models);
