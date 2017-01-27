a9e38be2181c35e19bc6f13d09afd0d6dc2a7aab
Scott Douglass <scott@scottyd.net>
2017-01-26 21:15:19 -0500

    Fixed some small issues and added styling for negative board

--- ui/app/pods/app/retrospectives/retrospective/sticky-notes/board-negative/route.js
+++ ui/app/pods/app/retrospectives/retrospective/sticky-notes/board-negative/route.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
+import config from '../../../../../../config/environment';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
+    session: Ember.inject.service('session'),
+    model() {
+        return RSVP.hash({
+            parent: this.modelFor('app.retrospectives.retrospective'),
+            feedback: this.modelFor('app.retrospectives.retrospective').retrospective.get('feedbacks')
+        });
+    },
+    setupController(controller, model) {
+        this._super(...arguments);
+
+        let _this = this,
+            feedback = model.feedback;
+
+        let negativeFeedback = feedback.filter((fb) => {
+            return fb.get('type') === "negative";
+        });
+
+        controller.set('negative_feedback', negativeFeedback);
+    },
+    
+    actions: {
+        
+    }
 });
