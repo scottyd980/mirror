fc1a26cf74645dceccbebda6eef415276abb545e
Scott Douglass <scott@scottyd.net>
2017-02-06 19:28:04 -0500

    Built out join functionality

--- ui/app/pods/app/teams/join/route.js
+++ ui/app/pods/app/teams/join/route.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    actions: {
+        joinTeam() {
+            let accessCode = this.controller.get('accessCode');
+            this.transitionTo('app.teams.access', accessCode);
+        }
+    }
 });
