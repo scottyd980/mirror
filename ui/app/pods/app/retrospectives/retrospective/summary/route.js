9add351fab8d79072c0702a95d25646c5f696f98
Scott Douglass <scott@scottyd.net>
2017-02-08 19:34:32 -0500

    Updating summary page with scores

--- ui/app/pods/app/retrospectives/retrospective/summary/route.js
+++ ui/app/pods/app/retrospectives/retrospective/summary/route.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
+import config from '../../../../../config/environment';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
+    model() {
+        return RSVP.hash({
+            parent: this.modelFor('app.retrospectives.retrospective'),
+            scores: this.modelFor('app.retrospectives.retrospective').retrospective.get('scores')
+        });
+    },
+    setupController(controller, model) {
+        this._super(...arguments);
+
+        let scores = model.scores.sortBy('score');
+
+        let average = scores.reduce((total, score) => {
+            return total + score.get('score');
+        }, 0) / scores.length;
+
+        controller.set('low_score', scores[0]);
+        controller.set('high_score', scores[scores.length - 1]);
+        controller.set('average_score', average);
+    }
 });
