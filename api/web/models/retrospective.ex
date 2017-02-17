70284f72e7d6ac5728641fd8df06323fdb700f3f
Scott Douglass <scott@scottyd.net>
2017-02-16 22:23:57 -0500

    Moving team retro checks to team instead of retrospective

--- api/web/models/retrospective.ex
+++ api/web/models/retrospective.ex
@@ -28,53 +28,8 @@ defmodule Mirror.Retrospective do
     |> validate_required([:name, :state, :isAnonymous, :team_id, :moderator_id, :type_id])
   end
 
-  def check_retrospective_in_progress(team) do
-    retro = get_retro_in_progress(team)
-
-    retro_count = retro
-    |> Enum.count
-
-    in_progress = retro_count > 0
-
-    {in_progress, retro}
-  end
-
   def preload_relationships(retrospective) do
     retrospective
     |> Repo.preload([:team, :moderator, :participants, :scores, :feedbacks, :type])
-  end
-
-  def get_retro_in_progress(team) do
-    retros = get_all_retrospectives(team)
-
-    retro_in_progress = retros
-    |> Enum.filter(fn(r) ->
-      retro_type = Repo.get!(RetrospectiveType, r.type_id)
-      r.state < retro_type.finished_state
-    end)
-    
-    retro_in_progress
-  end
-
-  def get_most_recent_retrospective(team) do
-    retros = get_all_retrospectives(team)
-    most_recent = Enum.sort(retros, fn(d1, d2) ->
-      Ecto.DateTime.to_erl(d1.updated_at) > Ecto.DateTime.to_erl(d2.updated_at)
-    end)
-    |> List.first
-  end
-
-  def get_next_retrospective(team) do
-    current_retro = get_most_recent_retrospective(team)
-    current_retro_number = current_retro.name
-    |> String.split(" ")
-    |> List.last
-    |> String.to_integer
-
-    current_retro_number + 1
-  end
-
-  def get_all_retrospectives(team) do
-    Repo.all(from retro in Retrospective, where: retro.team_id == ^team.id)
   end
 end
--- api/web/models/team.ex
+++ api/web/models/team.ex
@@ -1,7 +1,7 @@
 defmodule Mirror.Team do
   use Mirror.Web, :model
 
-  alias Mirror.{Repo}
+  alias Mirror.{Repo, Retrospective, RetrospectiveType}
 
   schema "teams" do
     field :name, :string
@@ -28,5 +28,50 @@ defmodule Mirror.Team do
   def preload_relationships(team) do
     team
     |> Repo.preload([:members, :admins])
+  end
+
+  def check_retrospective_in_progress(team) do
+    retro = get_retrospective_in_progress(team)
+
+    retro_count = retro
+    |> Enum.count
+
+    in_progress = retro_count > 0
+
+    {in_progress, retro}
+  end
+
+  def get_retrospective_in_progress(team) do
+    retros = get_all_retrospectives(team)
+
+    retro_in_progress = retros
+    |> Enum.filter(fn(r) ->
+      retro_type = Repo.get!(RetrospectiveType, r.type_id)
+      r.state < retro_type.finished_state
+    end)
+    
+    retro_in_progress
+  end
+
+  def get_most_recent_retrospective(team) do
+    retros = get_all_retrospectives(team)
+    most_recent = Enum.sort(retros, fn(d1, d2) ->
+      Ecto.DateTime.to_erl(d1.updated_at) > Ecto.DateTime.to_erl(d2.updated_at)
+    end)
+    |> List.first
+  end
+
+  def get_next_retrospective(team) do
+    current_retro = get_most_recent_retrospective(team)
+    current_retro_number = current_retro.name
+    |> String.split(" ")
+    |> List.last
+    |> String.to_integer
+
+    current_retro_number + 1
+  end
+
+  def get_all_retrospectives(team) do
+    Repo.all(from retro in Retrospective, where: retro.team_id == ^team.id)
   end
 end
