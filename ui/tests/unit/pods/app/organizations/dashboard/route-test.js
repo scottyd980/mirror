8649cdc800cc0a0e48099a0ed479102819415f54
Scott Douglass <scott@scottyd.net>
2017-02-24 20:42:58 -0500

    Created organization dashboard

---
+++ ui/tests/unit/pods/app/organizations/dashboard/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:app/organizations/dashboard', 'Unit | Route | app/organizations/dashboard', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
