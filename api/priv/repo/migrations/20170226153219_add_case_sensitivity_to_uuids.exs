84c1346761d9234603e7ab00a5282f00d207c08c
Scott Douglass <scott@scottyd.net>
2017-02-26 10:36:03 -0500

    Added migration for case-sensitivity in the uuid columns

---
+++ api/priv/repo/migrations/20170226153219_add_case_sensitivity_to_uuids.exs
@@ -0,0 +1,8 @@
+defmodule Mirror.Repo.Migrations.AddCaseSensitivityToUuids do
+  use Ecto.Migration
+
+  def change do
+    execute "ALTER TABLE organizations MODIFY uuid VARCHAR(255) COLLATE utf8_bin;"
+    execute "ALTER TABLE teams MODIFY uuid VARCHAR(255) COLLATE utf8_bin;"
+  end
+end
