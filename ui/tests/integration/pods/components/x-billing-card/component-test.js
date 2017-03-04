9043a9ba8d92ff4fe25015fbd6398483e1bc6472
Scott Douglass <scott@scottyd.net>
2017-03-04 15:41:31 -0500

    Credit card submission is working!

--- ui/tests/integration/pods/components/x-billing-card/component-test.js
+++ ui/tests/integration/pods/components/x-billing-card/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('stripe-billing', 'Integration | Component | stripe billing', {
+moduleForComponent('x-billing-card', 'Integration | Component | x billing card', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{stripe-billing}}`);
+  this.render(hbs`{{x-billing-card}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#stripe-billing}}
+    {{#x-billing-card}}
       template block text
-    {{/stripe-billing}}
+    {{/x-billing-card}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
---
+++ ui/tests/unit/helpers/cc-type-test.js
@@ -0,0 +1,12 @@
+
+import { ccType } from 'mirror/helpers/cc-type';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | cc type');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = ccType([42]);
+  assert.ok(result);
+});
+
