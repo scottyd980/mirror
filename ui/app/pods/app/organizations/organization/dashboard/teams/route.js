332ed0d57d41702b819e7d619604e470b58be769
Scott Douglass <scott@scottyd.net>
2017-03-10 18:16:41 -0500

    Added organization team page

--- ui/app/pods/app/organizations/organization/dashboard/teams/route.js
+++ ui/app/pods/app/organizations/organization/dashboard/teams/route.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
+    session: Ember.inject.service('session'),
+    model() {
+        var _this = this;
+        return RSVP.hash({
+            organization: this.modelFor('app.organizations.organization'),
+            currentUser: _this.get('session').get('currentUser'),
+            teams: this.modelFor('app.organizations.organization').get('teams')
+        });
+    },
+    setupController(controller, models) {
+        var _this = this;
+        _this._super(controller, models);
+
+        controller.set('isAdmin', models.organization.get('admins').includes(models.currentUser));
+    }
 });
