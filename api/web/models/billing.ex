02f6884b2ddd8e6aff3758f0fc47671630c2aafe
Scott Douglass <scott@scottyd.net>
2017-03-16 21:17:51 -0400

    Implemented check for active billing subscription

--- api/web/models/billing.ex
+++ api/web/models/billing.ex
@@ -1,7 +1,9 @@
 defmodule Mirror.Billing do
   use Mirror.Web, :model
 
-  alias Mirror.{Repo, Card, Organization}
+  import Logger
+
+  alias Mirror.{Repo, Card, Organization, Team, Helpers}
 
   def add_payment(customer, source) do
       Stripe.Cards.create(:customer, customer, %{source: source})
@@ -13,5 +15,30 @@ defmodule Mirror.Billing do
 
   def delete_payment(customer, source) do
       Stripe.Cards.delete(:customer, customer, source)
+  end
+
+  def is_active?(%Team{organization_id: organization_id}) do
+      Logger.warn "test1"
+      case organization_id do
+          nil -> false
+          _ -> is_active?(Repo.get!(Organization, organization_id))
+      end
+  end
+
+  def is_active?(%Organization{billing_customer: billing_customer}) do
+      Logger.warn "test2"
+      case billing_customer do
+          nil -> false
+          _ -> is_stripe_active?(billing_customer)
+      end
+  end
+
+  def is_stripe_active?(billing_customer) do
+      Logger.warn "test3"
+      {:ok, subscriptions} = Stripe.Subscriptions.all(billing_customer)
+      subscriptions = Helpers.atomic_map(subscriptions)
+
+      subscription = hd(subscriptions)
+      subscription.status == "trialing" || subscription.status == "active"
   end
 end
