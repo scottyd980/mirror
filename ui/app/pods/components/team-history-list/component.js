8984f8f7aa6cb51ca335ec2ac3753fc3ec9fbd69
Scott Douglass <scott@scottyd.net>
2017-06-25 19:52:55 -0400

    Fix the retrospective history order.

--- ui/app/pods/components/team-history-list/component.js
+++ ui/app/pods/components/team-history-list/component.js
@@ -3,6 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     tagName: "ul",
     classNames: ["table-list"],
-    // sortedMembers: Ember.computed.sort('members', 'memberSortDefinition'),
-    // memberSortDefinition: ['id:asc'],
+    sortedRetros: Ember.computed.sort('retrospectives', function(a, b){
+        let id1 = parseInt(a.id),
+            id2 = parseInt(b.id);
+
+        if (id1 < id2) {
+            return 1;
+        } else if (id1 > id2) {
+            return -1;
+        }
+
+        return 0;
+    })
 });
