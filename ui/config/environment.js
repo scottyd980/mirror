032733fc975cdf83d0a105fa6b191e1214de5850
Scott Douglass <scott@scottyd.net>
2018-02-19 15:24:48 -0500

    Make the flow generic so any step can be in any order.

--- ui/config/environment.js
+++ ui/config/environment.js
@@ -77,7 +77,7 @@ module.exports = function(environment) {
     retrospective: {
       sticky_notes: {
         type_id: 1,
-        states: ["start", "score", "aggregate", "feedback", "sticky-notes.board-negative", "sticky-notes.board-positive", "summary"],
+        states: ["start", "feedback", "sticky-notes.board-negative", "sticky-notes.board-positive", "score", "aggregate", "summary"],
         min_complete_state: 6,
         feedback: [
           {
