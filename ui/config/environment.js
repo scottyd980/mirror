48d3df43e223f110a4cb973bf6cb6fe20004702d
Scott Douglass <scott@scottyd.net>
2018-03-11 13:34:52 -0400

    Remove port 4000

--- ui/config/environment.js
+++ ui/config/environment.js
@@ -123,8 +123,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.DS.host = 'https://usemirror.io:4000';
-    ENV.DS.wshost = 'wss://usemirror.io:4000';
+    ENV.DS.host = 'https://usemirror.io';
+    ENV.DS.wshost = 'wss://usemirror.io';
   }
 
   return ENV;
