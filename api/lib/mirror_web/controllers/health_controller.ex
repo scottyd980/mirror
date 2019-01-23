f294bf0f6397307bc79164d8336f4181e91668b7
Scott Douglass <scott@scottyd.net>
2019-01-22 20:31:11 -0500

    [ci deploy api] redeploy with logger instead

--- api/lib/mirror_web/controllers/health_controller.ex
+++ api/lib/mirror_web/controllers/health_controller.ex
@@ -1,11 +1,15 @@
 defmodule MirrorWeb.HealthController do
   use MirrorWeb, :controller
 
+  require Logger
+
   action_fallback MirrorWeb.FallbackController
 
   def index(conn, _params) do
+    Logger.warn "#{inspect conn}"
+
     conn
     |> put_status(200)
-    |> json(%{status: "healthy", time: DateTime.utc_now, remote_ip: conn.remote_ip})
+    |> json(%{status: "healthy", time: DateTime.utc_now})
   end
 end
