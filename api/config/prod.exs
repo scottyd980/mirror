29beb7f40fca9dbab267632e1719d5c9d3926b00
Scott Douglass <scott@scottyd.net>
2019-01-22 20:17:11 -0500

    [ci deploy api] update url

--- api/config/prod.exs
+++ api/config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :mirror, MirrorWeb.Endpoint,
   http: [port: "${PORT}"],
-  check_origin: ["localhost", "//127.0.0.1", "127.0.0.1"],
+  url: [host: "127.0.0.1"],
   server: true
 
 # Do not print debug messages in production
