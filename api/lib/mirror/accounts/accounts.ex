09213557347933ae8c2520fd09e0187ce5aa1d0c
Scott Douglass <scott@scottyd.net>
2019-01-28 22:20:04 -0500

    [ci deploy both] Forgotten password functionality.

--- api/lib/mirror/accounts/accounts.ex
+++ api/lib/mirror/accounts/accounts.ex
@@ -8,6 +8,8 @@ defmodule Mirror.Accounts do
 
   alias Mirror.Accounts.User
 
+  @day 86400
+
   @doc """
   Returns the list of users.
 
@@ -50,6 +52,29 @@ defmodule Mirror.Accounts do
 
   """
   def get_user_by_email(email), do: Repo.get_by(User, email: email)
+
+  def get_user_by_reset_token(reset_token) do
+    case Repo.get_by(User, reset_password_token: reset_token) do
+        %User{} = user ->
+            case (user.reset_token_sent_at + @day) > DateTime.to_unix(DateTime.utc_now()) do
+                true -> user
+                false -> nil
+            end
+        nil -> nil
+    end
+  end
+
+  def begin_user_password_recovery(user) do
+    user
+    |> User.password_recovery_changeset()
+    |> Repo.update()
+  end
+
+  def reset_user_password(user, attrs) do
+    user
+    |> User.password_reset_changeset(attrs)
+    |> Repo.update()
+  end
 
   @doc """
   Gets a single user.
