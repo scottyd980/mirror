c0140feb689cf7aa654503eecb0711087ce2fe36
Scott Douglass <scott@scottyd.net>
2019-02-05 22:59:29 -0500

    Add billing route to team dashboard.

--- client/app/routes/app/teams/team/dashboard/billing.js
+++ client/app/routes/app/teams/team/dashboard/billing.js
@@ -1,4 +1,26 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
+import ENV from 'mirror/config/environment';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  session: service(),
+  model() {
+    return hash({
+      team: this.modelFor('app.teams.team'),
+      user_organizations: this.get('session.currentUser').get('organizations'),
+      // TODO: Remove?
+      currentUser: this.get('session').get('currentUser')
+    });
+  },
+  setupController(controller) {
+    this._super(...arguments);
+    controller.set('isJoinOwnOrganizationModalShowing', false);
+    controller.set('isCreateNewOrganizationModalShowing', false);
+    controller.set('isJoinAnotherOrganizationModalShowing', false);
+    controller.set('newOrganizationName', '');
+    controller.set('orgError', '');
+
+    controller.set('active_billing_types', ENV.ACTIVE_BILLING_TYPES);
+  },
 });
