7bc43c43e1afeae0ef94a361ad3bd776376fe020
Scott Douglass <scott@scottyd.net>
2019-02-05 20:55:30 -0500

    Add parent organization structure.

--- client/app/components/x-list/organization.js
+++ client/app/components/x-list/organization.js
@@ -4,5 +4,10 @@ import ENV from 'mirror/config/environment';
 export default Component.extend({
   tagName: "",
   hideDelete: false,
-  active_billing_types: ENV.ACTIVE_BILLING_TYPES
+  active_billing_types: ENV.ACTIVE_BILLING_TYPES,
+  actions: {
+    onConfirm(message, action) {
+      this.get('notifications').confirmAction(message, action);
+    }
+  }
 });
---
+++ client/app/routes/app/organizations/organization/dashboard.js
@@ -0,0 +1,19 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
+
+export default Route.extend({
+  session: service(),
+  model() {
+    return hash({
+      organization: this.modelFor('app.organizations.organization'),
+      //TODO: Remove?
+      currentUser: this.get('session').get('currentUser')
+    });
+  },
+  setupController(controller, models) {
+    this._super(controller, models);
+
+    controller.set('isAdmin', models.organization.get('admins').includes(models.currentUser));
+  }
+});
