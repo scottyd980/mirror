9e521839a208123ff9b4f2d1234f517093839bb3
Scott Douglass <scott@scottyd.net>
2019-02-10 22:21:32 -0500

    [ci deploy both] Remove unnecessary migration.

--- api/priv/repo/migrations/20190209004559_add_active_feedback_id_to_retrsopective.exs
+++
@@ -1,9 +0,0 @@
-defmodule Mirror.Repo.Migrations.AddActiveFeedbackIdToRetrsopective do
-  use Ecto.Migration
-
-  def change do
-    alter table(:retrospectives) do
-      add :active_feedback_id, references(:retrospective_feedbacks)
-    end
-  end
-end
