ac2241513f93d4a585f49305ea0c890dfb269365
Scott Douglass <scott@scottyd.net>
2019-03-07 22:03:34 -0500

    [ci deploy api] Update ssl.

--- api/lib/mirror/repo.ex
+++ api/lib/mirror/repo.ex
@@ -8,15 +8,11 @@ defmodule Mirror.Repo do
   DATABASE_URL environment variable.
   """
   def init(_, config) do
-    if config[:ssl] do
-      cert_dir = Application.app_dir(:mirror, "priv/ssl")
-      ssl_opts = [cacertfile: Path.join(cert_dir, "ca.pem"),
-                  certfile: Path.join(cert_dir, "client-cert.pem"),
-                  keyfile: Path.join(cert_dir, "client-key.pem")]
-      opts = [ssl_opts: ssl_opts] ++ config
-      {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
-    else
-      {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
-    end
+    cert_dir = Application.app_dir(:mirror, "priv/ssl")
+    ssl_opts = [cacertfile: Path.join(cert_dir, "ca.pem"),
+                certfile: Path.join(cert_dir, "client-cert.pem"),
+                keyfile: Path.join(cert_dir, "client-key.pem")]
+    opts = [ssl_opts: ssl_opts] ++ config
+    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
   end
 end
