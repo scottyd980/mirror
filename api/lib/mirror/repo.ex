3d7024f205462b4c9ac5ba1f9c520f9a447ff7b8
Scott Douglass <scott@scottyd.net>
2019-03-07 21:02:00 -0500

    [ci deploy api] Update repo to use correct pem files.

--- api/lib/mirror/repo.ex
+++ api/lib/mirror/repo.ex
@@ -7,7 +7,16 @@ defmodule Mirror.Repo do
   Dynamically loads the repository url from the
   DATABASE_URL environment variable.
   """
-  def init(_, opts) do
-    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+  def init(_, config) do
+    if config[:ssl] do
+      cert_dir = Application.app_dir(:mirror, "priv/ssl")
+      ssl_opts = [cacertfile: Path.join(cert_dir, "ca.pem"),
+                  certfile: Path.join(cert_dir, "client-cert.pem"),
+                  keyfile: Path.join(cert_dir, "client-key.pem")]
+      opts = [ssl_opts: ssl_opts] ++ config
+      {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+    else
+      {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
+    end
   end
 end
