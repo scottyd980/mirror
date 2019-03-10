fa0d1faa3d226d180c89a68e81e5bef0ef12b6fe
Scott Douglass <scott@scottyd.net>
2019-03-09 21:41:29 -0500

    [ci deploy api] fix DB.

--- api/lib/mirror/repo.ex
+++ api/lib/mirror/repo.ex
@@ -9,15 +9,15 @@ defmodule Mirror.Repo do
   """
   #
   def init(_, config) do
-    if config[:ssl] do
-      cert_dir = Application.app_dir(:mirror, "priv/ssl")
-      ssl_opts = [cacertfile: Path.join(cert_dir, "ca.pem"),
-                  certfile: Path.join(cert_dir, "client-cert.pem"),
-                  keyfile: Path.join(cert_dir, "client-key.pem")]
-      opts = [ssl_opts: ssl_opts] ++ config
-      {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
-    else
+    # if config[:ssl] do
+    #   cert_dir = Application.app_dir(:mirror, "priv/ssl")
+    #   ssl_opts = [cacertfile: Path.join(cert_dir, "ca.pem"),
+    #               certfile: Path.join(cert_dir, "client-cert.pem"),
+    #               keyfile: Path.join(cert_dir, "client-key.pem")]
+    #   opts = [ssl_opts: ssl_opts] ++ config
+    #   {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+    # else
       {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
-    end
+    # end
   end
 end
