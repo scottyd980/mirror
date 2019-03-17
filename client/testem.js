a42fb9b3a08a5a9caa246b3b88174c40dee36890
Scott Douglass <scott@scottyd.net>
2019-03-17 13:40:55 -0400

    Remove --no-sandbox flag.

--- client/testem.js
+++ client/testem.js
@@ -11,15 +11,14 @@ module.exports = {
     Chrome: {
       ci: [
         // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
+        //process.env.CI ? '--no-sandbox' : null,
         '--headless',
         '--disable-gpu',
         '--disable-dev-shm-usage',
         '--disable-software-rasterizer',
         '--mute-audio',
         '--remote-debugging-port=0',
-        '--window-size=1440,900',
-        '--user-data-dir'
+        '--window-size=1440,900'
       ].filter(Boolean)
     }
   }
