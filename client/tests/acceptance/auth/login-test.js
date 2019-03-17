d2eb6d09caf0e1bacbd931a4cf480c21b721b700
Scott Douglass <scott@scottyd.net>
2019-03-17 13:12:08 -0400

    Add tests and chromium.

--- client/tests/acceptance/auth/login-test.js
+++ client/tests/acceptance/auth/login-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, fillIn, click, find, waitFor } from '@ember/test-helpers';
+import { visit, currentURL, fillIn, click, find, waitFor, findAll } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import scenarios from "mirror/mirage/scenarios";
@@ -36,5 +36,32 @@ module('Acceptance | auth/login', function(hooks) {
 
     assert.ok(find(".alert-danger"))
     assert.notEqual(currentURL(), '/app');
+  });
+
+  test('register link works', async function(assert) {
+    await visit('/auth/login');
+    const registerLink = findAll('a').find((element) => {
+      return element.textContent.trim() === 'Don\'t have an account yet?';
+    });
+    await click(registerLink);
+    assert.equal(currentURL(), '/auth/register');
+  });
+
+  test('forgotten password link works', async function(assert) {
+    await visit('/auth/login');
+    const passwordLink = findAll('a').find((element) => {
+      return element.textContent.trim() === 'password';
+    });
+    await click(passwordLink);
+    assert.equal(currentURL(), '/auth/forgot/password');
+  });
+
+  test('forgotten username link works', async function(assert) {
+    await visit('/auth/login');
+    const usernameLink = findAll('a').find((element) => {
+      return element.textContent.trim() === 'username';
+    });
+    await click(usernameLink);
+    assert.equal(currentURL(), '/auth/forgot/username');
   });
 });
